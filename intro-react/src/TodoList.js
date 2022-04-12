93591871ad7a6e36dd913cd928ec2f60e33455bd
unknown <cskeith01@gmail.com>
2022-04-12 11:18:33 +0200

    Trying to useState

--- intro-react/src/TodoList.js
+++ intro-react/src/TodoList.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function TodoList() {
-    const todos = ["Afwassen", "Auto Opruimen"];
+    const todos = ['Afwassen', 'Auto Opruimen'];
+    const todoItems = todos.map((todo) =>
+    <li key={todo}>
+      <input type="checkbox" />{todo}
+    </li>
+    );
   return (
     <ul>
-    {todos.map((todo) => (
-      <li>
-        <input type="checkbox" /> {todo}
-      </li>
-    ))}
-  </ul>
-  )
+      {todoItems}
+    </ul>
+  );
 }
