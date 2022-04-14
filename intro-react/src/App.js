d2430923e6c560c3da8dcb731cfb59c8b4be0af2
unknown <cskeith01@gmail.com>
2022-04-14 15:09:52 +0200

    Wip Save data

--- intro-react/src/App.js
+++ intro-react/src/App.js
@@ -2,17 +2,27 @@ import {useState, useRef, useEffect} from "react";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from 'uuid';
 
+const STORE_KEY = 'todoApp.todos'
 
 function App() {
  const [todos, setTodos ] = useState([])
  const todoNameRef = useRef()
+
+ useEffect(() => { // Try to Get data
+  const storedTodos = JSON.parse(localStorage.getItem(STORE_KEY)) 
+  if (storedTodos) setTodos(storedTodos)
+ },[])
+
+ useEffect(() => { // Try to save data
+  localStorage.setItem(STORE_KEY, JSON.stringify(todos))
+ }, [todos])
 
  function handleAddTodo(e) {
  const name = todoNameRef.current.value
  if(name === '') 
   return
   setTodos(prevTodos => {
-    return [...prevTodos, { id: uuidv4(), name: name, complete: true}
+    return [...prevTodos, { id: uuidv4(), name: name, complete: false}
     ]})
     console.log(name)
     todoNameRef.current.value = null
