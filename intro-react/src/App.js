c23afbc58cd5a0cd0644e808b2d759b8484860ed
unknown <cskeith01@gmail.com>
2022-04-14 11:00:36 +0200

    WIP catch DATA

--- intro-react/src/App.js
+++ intro-react/src/App.js
@@ -1,31 +1,36 @@
 import {useState, useRef, useEffect} from "react";
 import TodoList from "./components/TodoList";
-import Form from "./Form";
 
 
 function App() {
- const [todos, setTodos] = useState([]); // var to catch a state
+ const [todos, setTodos] = useState([""]); // var to catch a state
  const newTodo = useRef();  // a way to follow changes in elements or text
  const handleChange = () => { // change handeler
-   console.log(newTodo.current.value);
+    // setTodos(evt.currentTarget.value);
+  //  setTodos(newTodo.current.value); 
+  
  }
+
+ const onChangeHandler = event => {
+  setInputValue(event.target.value);
+};
  
 useEffect(() => {
  let storedTodo = JSON.parse(localStorage.getItem('myTodo'));
- console.log(storedTodo);
  if(storedTodo){
   setTodos(storedTodo);
  }
 },[]);
- console.log(todos);
-
 
   return (
     <>
-    <Form />
+    <input type="text" name="todoField"    />
+    <button onChange={onChangeHandler}
+        value={todos}>ADD Todo</button>
+    
     <TodoList todos={todos}/>
+
     </>
-  
   )
 }
 
