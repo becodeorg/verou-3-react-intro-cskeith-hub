d4c59356510a691bee26b518011b149c556f8cae
unknown <cskeith01@gmail.com>
2022-04-19 17:02:10 +0200

    WIP Mobile View Style

--- intro-react/src/App.js
+++ intro-react/src/App.js
@@ -6,51 +6,54 @@ import "./Styles/main.css"
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
- const [todos, setTodos ] = useState([])
- const todoNameRef = useRef()
+  const todoNameRef = useRef()
+  const [todos, setTodos ] = useState([])
 
- useEffect(() => {
+  useEffect(() => {
   const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
   if (storedTodos?.length) setTodos(storedTodos)
- 
- },[])
 
- useEffect(() => { // Try to save data
+  },[])
+
+  useEffect(() => { // Try to save data
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
- }, [todos])
+  }, [todos])
 
+  function handleAddTodo(e) {
+      const name = todoNameRef.current.value
+      if(name === "") 
+       return
+       setTodos(prevTodos => {
+         return [...prevTodos, { id: uuidv4(), name: name, complete: false}
+         ]})
+         todoNameRef.current.value = null
+      }
 
- function toggleTodo(id) {
-  const newTodos = [...todos]
-  const todo = newTodos.find(todo => todo.id === id)
-  todo.complete = !todo.complete
-  setTodos(newTodos)
- }
+  function handleClearTodos() {
+      const newTodos = todos.filter(todo => !todo.complete)
+      setTodos(newTodos)
+    }
 
- function handleAddTodo(e) {
- const name = todoNameRef.current.value
- if(name === "") 
-  return
-  setTodos(prevTodos => {
-    return [...prevTodos, { id: uuidv4(), name: name, complete: false}
-    ]})
-    todoNameRef.current.value = null
- }
-
- function handleClearTodos() {
-   const newTodos = todos.filter(todo => !todo.complete)
-   setTodos(newTodos)
- }
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+   }
   return (
     <>
       <h1>Task Manager</h1>
-      <div className="countTodo">{todos.filter(todo => !todo.complete).length} left to do</div>
-
-      <TodoList todos={todos} toggleTodo={toggleTodo}/>
-      <input className="inputField" ref={todoNameRef} type="text"></input>
-      <button className="addButton" onClick={handleAddTodo}>Add Todo</button>
-      <button className="clearButton" onClick={handleClearTodos}>Clear Completed</button>
-      
+      <div className="todoContainer">
+        <div className="countTodo">{todos.filter(todo => !todo.complete).length} left to do</div>
+        <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      </div>
+     
+      <div className="form">
+        <input className="inputField" ref={todoNameRef} type="text"></input>
+        <button className="addButton" onClick={handleAddTodo}>Add Todo</button>
+        <button className="clearButton" onClick={handleClearTodos}>Clear Completed</button>
+      </div>
+     
     </>
     
   )
---
+++ intro-react/src/Form.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+function Form() {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Form
